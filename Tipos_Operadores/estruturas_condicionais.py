6ed8a17724d45adff4447f7a2335465e2464eb0a
Bruno H Marinho Marques <brunohenriw1@gmail.com>
2025-12-04 15:59:31 -0300

    Estruturas condicionais

---
+++ Tipos_Operadores/estruturas_condicionais.py
@@ -0,0 +1,23 @@
+MAIOR_IDADE = 18
+IDADE_AULAS_TEORICAS = 15
+idade = int(input("Informe sua idade:"))
+
+if idade >=MAIOR_IDADE:
+    print("Maior de idade pode tirar a CNH.")
+
+if idade < MAIOR_IDADE:
+    print("Ainda não pode tirar a CNH.")
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Ainda não pode tirar!")
+
+
+if idade>= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+elif idade == IDADE_AULAS_TEORICAS:
+    print("Pode fazer as aulas teóricas, mas não as práticas")
+
+else:
+    print("Ainda não pode tirar a CNH.")
